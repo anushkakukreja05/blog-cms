e24e5a649c4535540fbb92c6be313d99dcb211f7
Anushka Kukreja <kukrejaanushka05@gmail.com>
2024-04-11 17:17:25 +0530

    postcontroller

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -13,4 +13,61 @@ class PostsController extends Controller
 
         return view('admin-panel.posts.index',compact(['posts']));
     }
+
+    public function create() {
+        $categories = Category::all();
+        $tags = Tag::all();
+        return view('admin-panel.posts.create',compact(['categories','tags']));
+    }
+
+    public function store(CreatePostRequest $request) {
+        $image = $request->file('image')->store('images/posts');
+        $post = Post::create([
+            'title'=>$request->title,
+            'excerpt'=>$request->excerpt,
+            'body'=>$request->body,
+            'published_at'=>$request->published_at,
+            'category_id'=>$request->category_id,
+            'image_path'=>$image,
+            'user_id'=> 1
+        ]);
+
+        $post->tags()->attach($request->tags);
+
+        return redirect(route('posts.index'))->with('success','Post Created successfully!');
+    }
+
+    public function edit(Request $request, Post $post) {
+        $categories = Category::all();
+        $tags = Tag::all();
+        return view('admin-panel.posts.edit',compact(['post','categories','tags']));
+    }
+
+    public function update(Request $request,Post $post) {
+        $data = $request->only(['title','excerpt','body','published_at','category_id']);
+
+        if($request->hasFile('image')) {
+            $data['image_path'] = $request->file('image')->store('images/posts');
+            $post->deleteImage();
+        }
+        $post->update($data);
+        $post->tags()->sync($request->tags);
+
+        session()->flash('success','Post updated successfully!');
+        return(redirect(route('posts.index')));
+    }
+
+    public function publish (Request $request,Post $post) {
+        $post->published_at = now();
+        $post->save();
+        session()->flash('success','Post updated successfully!');
+        return (redirect(route('posts.index')));
+    }
+
+    public function destroy (Post $post) {
+        $post->deleteImage();
+        $post->delete();
+        session()->flash('success','Post updated successfully!');
+        return (redirect(route('posts.index')));
+    }
 }
